b8bc0a9ea27217529bb71a4e066ef51225e64f20
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-17 08:30:48 -0400

    update

--- front/src/components/navbar/Navbar.js
+++ front/src/components/navbar/Navbar.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Container from "@mui/material/Container";
+import { Box, Drawer, Typography } from "@mui/material";
 
 const Navbar = () => {
+  const [openMobileMenu, setOpenMobileMenu] = useState(false);
+
   return (
     <div>Navbar</div>
   )
